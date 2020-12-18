0cf2fccbe378270b4d9df50e0b7b9f2d86a3dbf4
Roosevelt <panta_85.x@hotmail.com>
2020-12-18 18:28:17 -0500

    fixed swap hours minutes

--- src/utils/now.js
+++ src/utils/now.js
@@ -1,8 +1,8 @@
 function now() {
   const currTime = new Date();
-  const minutes = String(currTime.getMinutes()).padStart(2, '0');
   const hours = String(currTime.getHours()).padStart(2, '0');
-  return `${minutes}:${hours}`;
+  const minutes = String(currTime.getMinutes()).padStart(2, '0');
+  return `${hours}:${minutes}`;
 }
 
 export default now;
