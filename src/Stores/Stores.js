a7f83185917b023a62b5e31138e1b1aad8e46a8c
Roosevelt <panta_85.x@hotmail.com>
2022-05-07 18:10:06 -0500

    sort stores by sales

--- src/Stores/Stores.js
+++ src/Stores/Stores.js
@@ -1,44 +1,48 @@
-import './Stores.scss';
+import "./Stores.scss";
 import StoreLine from "./StoreLine/StoreLine";
 
-function Stores({sales, view}) {
-  const totalSales = sales.reduce((acc, curr) => acc += curr['amount'] , 0);
+function Stores({ sales, view }) {
+  const totalSales = sales.reduce((acc, curr) => (acc += curr["amount"]), 0);
   let salesFormatted;
   switch (view) {
-    case 'p':
+    case "p":
       salesFormatted = (storeSales) => {
         return `${((storeSales / totalSales) * 100).toFixed(2)} %`;
       };
       break;
-    case 'k':
+    case "k":
       salesFormatted = (storeSales) => {
         return `${(storeSales / 1000).toFixed(1)}K`;
       };
       break;
-    case 'n':
+    case "n":
       salesFormatted = (storeSales) => {
         return `S/ ${storeSales.toFixed(2)}`;
       };
       break;
   }
 
-    return (
-      <div>
-        {sales.map(({code, name, amount}) => (
+  return (
+    <div>
+      {sales
+        .sort((storeA, storeB) => storeB.amount - storeA.amount)
+        .map(({ code, name, amount }) => (
           <StoreLine
-              key={code}
-              bgColor={code}
-              storeName={name}
-              salesAmount={salesFormatted(amount)}
-              view={view}/>
+            key={code}
+            bgColor={code}
+            storeName={name}
+            salesAmount={salesFormatted(amount)}
+            view={view}
+          />
         ))}
-        <StoreLine
-            bgColor={'tt-store'}
-            storeName={'total'}
-            salesAmount={salesFormatted(totalSales)}
-            view={view}/>
-      </div>
-    );
-  }
+      <StoreLine
+        bgColor={"tt-store"}
+        storeName={"total"}
+        salesAmount={salesFormatted(totalSales)}
+        view={view}
+      />
+    </div>
+  );
+}
 
-  export default Stores;
+export default Stores;
